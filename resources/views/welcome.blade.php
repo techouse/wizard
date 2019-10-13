18f7b71df5ab7a20b838976b9e85e97ecfbb4fe2
Klemen Tušar <techouse@gmail.com>
2019-10-13 12:38:21 +0200

    :wrench: Change the welcome page's header some more

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="shortcut icon" type="image/png" href="/images/favicon.png" />
 
-    <title>Wizard</title>
+    <title>{{ $title ?? env('APP_NAME', 'Wizard') }}</title>
 
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
