322a77e2ec74f1c1c87dd5476efbc5b985fa7855
Klemen Tušar <techouse@gmail.com>
2019-10-13 12:36:47 +0200

    :wrench: Change the welcome page's title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,12 +4,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Wizard</title>
 
-    <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-    <!-- Styles -->
     <style>
         html, body {
             background-color: #fff;
