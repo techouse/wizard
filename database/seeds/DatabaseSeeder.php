696a6da37a4a60d0e39efa3407e8cfcdf1d04670
Konstantin Schmidt <31983699+Assimilationstheorie@users.noreply.github.com>
2019-10-19 18:19:31 +0200

    Update DatabaseSeeder.php
    Now you can user "php artisan db:seed"

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        factory('App\User', 3)->create();
         // $this->call(UsersTableSeeder::class);
     }
 }
