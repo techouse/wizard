676fc2e409606a39678d0d17a3a8dc16337069d9
Konstantin Schmidt <31983699+Assimilationstheorie@users.noreply.github.com>
2019-10-19 18:21:18 +0200

    Update AdminController.php
    Only auth people can see this site

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,9 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
+        /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         return view('admin');
