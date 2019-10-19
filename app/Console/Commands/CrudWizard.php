265498c7c30884bd029e40bf4735271aa9856613
Klemen Tušar <techouse@gmail.com>
2019-10-19 20:36:00 +0200

    :sparkles: Add JS resource generation to CRUD wizard

--- app/Console/Commands/CrudWizard.php
+++ app/Console/Commands/CrudWizard.php
@@ -35,10 +35,8 @@ class CrudWizard extends Command
 
     /**
      * Execute the console command.
-     *
-     * @return mixed
      */
-    public function handle()
+    public function handle(): void
     {
         $name = ucfirst(Str::camel($this->argument('name')));
 
@@ -48,18 +46,37 @@ class CrudWizard extends Command
         $this->policy($name);
         $this->controller($name);
 
-        File::append(
-            base_path('routes/api.php'),
-            'Route::apiResource(\'' . Str::kebab(Str::plural($name)) . "', '{$name}Controller'); // TODO put this line in the right place"
-        );
+        // JS
+        $this->jsModel($name);
+        $this->jsModule($name);
+        $this->jsRoute($name);
+        if (!File::exists(resource_path('js/pages/' . ucfirst(Str::camel(Str::plural($name)))))) {
+            File::makeDirectory(resource_path('js/pages/' . ucfirst(Str::camel(Str::plural($name)))));
+        }
+        $this->jsPageCreate($name);
+        $this->jsPageEdit($name);
+        $this->jsPageIndex($name);
+        $this->jsPageList($name);
+
+        $this->warn('Do not forget to register the routes in ' . app_path('routes/api.php') . ' and ' . resource_path('js/router/index.js'). '. You might also want to make a menu entry in ' . resource_path('js/pages/Admin.vue') . '.');
     }
 
-    protected function getStub($type)
+    protected function getStub(string $type): string
     {
-        return file_get_contents(resource_path("stubs/$type.stub"));
+        return File::get(resource_path("stubs/$type.stub"));
     }
 
-    protected function model($name): void
+    protected function getStubJs(string $type): string
+    {
+        return File::get(resource_path("stubs/js/$type.stub"));
+    }
+
+    protected function getStubJsPage(string $type): string
+    {
+        return File::get(resource_path("stubs/js/Page/$type.stub"));
+    }
+
+    protected function model(string $name): void
     {
         $modelTemplate = str_replace(
             ['{{modelName}}'],
@@ -67,10 +84,12 @@ class CrudWizard extends Command
             $this->getStub('Model')
         );
 
-        file_put_contents(app_path('/' . ucfirst(Str::camel($name)) . '.php'), $modelTemplate);
+        File::put(app_path(ucfirst(Str::camel($name)) . '.php'), $modelTemplate);
+
+        $this->info('Generated file: ' . app_path(ucfirst(Str::camel($name)) . '.php'));
     }
 
-    protected function controller($name): void
+    protected function controller(string $name): void
     {
         $controllerTemplate = str_replace(
             [
@@ -88,10 +107,12 @@ class CrudWizard extends Command
             $this->getStub('Controller')
         );
 
-        file_put_contents(app_path('/Http/Controllers/Api/' . ucfirst(Str::camel($name)) . 'Controller.php'), $controllerTemplate);
+        File::put(app_path('Http/Controllers/Api/' . ucfirst(Str::camel($name)) . 'Controller.php'), $controllerTemplate);
+
+        $this->info('Generated file: ' . app_path('Http/Controllers/Api/' . ucfirst(Str::camel($name)) . 'Controller.php'));
     }
 
-    protected function policy($name): void
+    protected function policy(string $name): void
     {
         $policyTemplate = str_replace(
             [
@@ -105,10 +126,12 @@ class CrudWizard extends Command
             $this->getStub('Policy')
         );
 
-        file_put_contents(app_path('/Policies/' . ucfirst(Str::camel($name)) . 'Policy.php'), $policyTemplate);
+        File::put(app_path('Policies/' . ucfirst(Str::camel($name)) . 'Policy.php'), $policyTemplate);
+
+        $this->info('Generated file: ' . app_path('Policies/' . ucfirst(Str::camel($name)) . 'Policy.php'));
     }
 
-    protected function resource($name): void
+    protected function resource(string $name): void
     {
         $resourceTemplate = str_replace(
             [
@@ -120,10 +143,12 @@ class CrudWizard extends Command
             $this->getStub('Resource')
         );
 
-        file_put_contents(app_path('/Http/Resources/' . ucfirst(Str::camel($name)) . 'Resource.php'), $resourceTemplate);
+        File::put(app_path('Http/Resources/' . ucfirst(Str::camel($name)) . 'Resource.php'), $resourceTemplate);
+
+        $this->info('Generated file: ' . app_path('Http/Resources/' . ucfirst(Str::camel($name)) . 'Resource.php'));
     }
 
-    protected function resources($name): void
+    protected function resources(string $name): void
     {
         $resourcesTemplate = str_replace(
             [
@@ -139,6 +164,155 @@ class CrudWizard extends Command
             $this->getStub('Resources')
         );
 
-        file_put_contents(app_path('/Http/Resources/' . ucfirst(Str::camel(Str::plural($name))) . 'Resource.php'), $resourcesTemplate);
+        File::put(app_path('Http/Resources/' . ucfirst(Str::camel(Str::plural($name))) . 'Resource.php'), $resourcesTemplate);
+
+        $this->info('Generated file: ' . app_path('Http/Resources/' . ucfirst(Str::camel(Str::plural($name))) . 'Resource.php'));
+    }
+
+    protected function jsModel(string $name): void
+    {
+        $template = str_replace(
+            [
+                '##modelName##'
+            ],
+            [
+                $name
+            ],
+            $this->getStubJs('Model')
+        );
+
+        File::put(resource_path('js/models/' . ucfirst(Str::camel($name)) . '.js'), $template);
+
+        $this->info('Generated file: ' . resource_path('js/models/' . ucfirst(Str::camel($name)) . '.js'));
+    }
+
+    protected function jsModule(string $name): void
+    {
+        $template = str_replace(
+            [
+                '##modelName##',
+                '##modelNamePluralKebabCase##',
+                '##modelNamePluralPascalCase##',
+            ],
+            [
+                $name,
+                Str::kebab(Str::plural($name)),
+                ucfirst(Str::camel(Str::plural($name))),
+            ],
+            $this->getStubJs('Module')
+        );
+
+        File::put(resource_path('js/store/modules/' . Str::camel($name) . '.js'), $template);
+
+        $this->info('Generated file: ' . resource_path('js/store/modules/' . Str::camel($name) . '.js'));
+    }
+
+    protected function jsRoute(string $name): void
+    {
+        $template = str_replace(
+            [
+                '##modelName##',
+                '##modelNamePluralKebabCase##',
+                '##modelNamePluralPascalCase##',
+            ],
+            [
+                $name,
+                Str::kebab(Str::plural($name)),
+                ucfirst(Str::camel(Str::plural($name))),
+            ],
+            $this->getStubJs('Route')
+        );
+
+        File::put(resource_path('js/router/routes/' . Str::camel(Str::plural($name)) . '.js'), $template);
+
+        $this->info('Generated file: ' . resource_path('js/router/routes/' . Str::camel(Str::plural($name)) . '.js'));
+    }
+
+    protected function jsPageCreate(string $name): void
+    {
+        $template = str_replace(
+            [
+                '##modelName##',
+                '##modelNameLowerWithSpaces##',
+                '##modelNameKebabCase##',
+                '##modelNameCamelCase##',
+            ],
+            [
+                $name,
+                str_replace('_', ' ', Str::snake($name)),
+                Str::kebab($name),
+                Str::camel($name)
+            ],
+            $this->getStubJsPage('Create')
+        );
+
+        File::put(resource_path('js/pages/' . ucfirst(Str::camel(Str::plural($name))) . '/create.vue'), $template);
+
+        $this->info('Generated file: ' . resource_path('js/pages/' . ucfirst(Str::camel(Str::plural($name))) . '/create.vue'));
+    }
+
+    protected function jsPageEdit(string $name): void
+    {
+        $template = str_replace(
+            [
+                '##modelName##',
+                '##modelNameKebabCase##',
+                '##modelNameCamelCase##',
+                '##modelNameCapitalisedWithSpaces##',
+                '##modelNamePluralPascalCase##',
+            ],
+            [
+                $name,
+                Str::kebab($name),
+                Str::camel($name),
+                ucfirst(str_replace('_', ' ', Str::snake($name))),
+                ucfirst(Str::camel(Str::plural($name)))
+            ],
+            $this->getStubJsPage('Edit')
+        );
+
+        File::put(resource_path('js/pages/' . ucfirst(Str::camel(Str::plural($name))) . '/edit.vue'), $template);
+
+        $this->info('Generated file: ' . resource_path('js/pages/' . ucfirst(Str::camel(Str::plural($name))) . '/edit.vue'));
+    }
+
+    protected function jsPageIndex(string $name): void
+    {
+        $template = str_replace(
+            [
+                '##modelNamePluralPascalCase##'
+            ],
+            [
+                ucfirst(Str::camel(Str::plural($name)))
+            ],
+            $this->getStubJsPage('Index')
+        );
+
+        File::put(resource_path('js/pages/' . ucfirst(Str::camel(Str::plural($name))) . '/index.vue'), $template);
+
+        $this->info('Generated file: ' . resource_path('js/pages/' . ucfirst(Str::camel(Str::plural($name))) . '/index.vue'));
+    }
+
+    protected function jsPageList(string $name): void
+    {
+        $template = str_replace(
+            [
+                '##modelName##',
+                '##modelNameLowerWithSpaces##',
+                '##modelNamePluralPascalCase##',
+                '##modelNameCamelCase##',
+            ],
+            [
+                $name,
+                str_replace('_', ' ', Str::snake($name)),
+                ucfirst(Str::camel(Str::plural($name))),
+                Str::camel($name),
+            ],
+            $this->getStubJsPage('List')
+        );
+
+        File::put(resource_path('js/pages/' . ucfirst(Str::camel(Str::plural($name))) . '/list.vue'), $template);
+
+        $this->info('Generated file: ' . resource_path('js/pages/' . ucfirst(Str::camel(Str::plural($name))) . '/list.vue'));
     }
 }
